3c7825bf497712cc6e6f120bc3795af40eb59ba4
Tyler Blocksberg <tyler@songtrust.com>
2020-11-15 16:05:02 -0500

    commit all  build files for now

---
+++ frontend/build/root/precache-manifest.f1e014008b56726f82d86c3eecbbceeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4534653f7ca6875dce1788260f0d4ea0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1534c7b529e1a91791b1",
+    "url": "/static/css/main.773ad31c.chunk.css"
+  },
+  {
+    "revision": "f931b4ddd236e68a2716",
+    "url": "/static/js/2.571cc93c.chunk.js"
+  },
+  {
+    "revision": "1534c7b529e1a91791b1",
+    "url": "/static/js/main.c4ae2403.chunk.js"
+  },
+  {
+    "revision": "068434ef18f057673ef9",
+    "url": "/static/js/runtime-main.05c24115.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
